7d680c54b9e79c6520290c47fb9040c34e9d2f63
krspznk <poznyakh24@gmail.com>
2023-11-08 10:44:48 +0200

    add lab2_js

---
+++ lab2_js/script.js
@@ -0,0 +1,56 @@
+// Функція, яка змінює малюнок при наведенні миші
+function changeImage() {
+    var image = document.querySelector('.cat'); // Використовуємо клас "cat"
+    image.src = './img/cat2.jpg'; // Шлях до нового малюнка
+}
+// Функція, яка відновлює початковий вигляд малюнка
+function restoreImage() {
+    var image = document.querySelector('.cat'); // Використовуємо клас "cat"
+    image.src = './img/cat.jpg'; // Шлях до початкового малюнка
+}
+
+// Функція для перевірки міста та виведення результату
+function checkCity() {
+    const phoneNumber = document.getElementById("phoneNumber").value;
+    
+    const city = document.getElementById("city").value;
+
+    if (phoneNumber.trim() === "" || city.trim() === "") {
+        alert("Обидва поля обов'язкові для заповнення");
+        return;
+    }
+
+    var dictionary = {
+        "Київ": "10000",
+        "Львів": "10001",
+        "Одеса": "10002",
+        "Харків": "10003",
+        "Дніпро": "10004",
+        "Запоріжжя": "10005",
+        "Донецьк": "10006",
+        "Херсон": "10007",
+        "Полтава": "10008",
+        "Черкаси": "10009",
+        "Вінниця": "10010",
+        "Житомир": "10011",
+        "Чернігів": "10012",
+        "Суми": "10013",
+        "Кропивницький": "10014",
+        "Івано-Франківськ": "10015",
+        "Ужгород": "10016",
+        "Чернівці": "10017",
+        "Тернопіль": "10018",
+        "Луцьк": "10019",
+        "Рівне": "10020",
+        "Хмельницький": "10021",
+        "Луцьк": "10022",
+    };
+
+
+    if (city in dictionary) {
+        document.getElementById('result').innerText = 'Код міста ' + dictionary[city] + ': ' + phoneNumber;
+    } else {
+        document.getElementById('result').innerText = 'Місто ' + city + ' не підтримується';
+    }
+}
+
